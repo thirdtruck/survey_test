de3bac5b0b9a5e5d6a902d557348bfd04409fee2
James Holder <github.com@thirdtruck.org>
2015-01-09 19:42:35 -0500

    Add initial Sequelize files. Initialize MySQL database on server boot.

---
+++ models/question.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  var Question = sequelize.define("Question", {
+    title: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Question.hasMany(models.Answer);
+      }
+    }
+  });
+  return Question;
+};
