1da455aae9489373177bf3b375112161c76090f1
James Holder <github.com@thirdtruck.org>
2015-01-10 14:18:06 -0500

    Just delete questions before each test. Only drop/create all tables once. Add question creation with title test.

--- test/test.js
+++ test/test.js
@@ -4,9 +4,17 @@ var models = require('../models');
 var Question = models.Question;
 
 describe('Question', function() {
-  beforeEach(function(done) {
+  before(function(done) {
     models.sequelize
       .sync({ force: true })
+      .complete(function(err) {
+        done(err);
+      });
+  });
+
+  beforeEach(function(done) {
+    models.Question
+      .destroy({ where: true })
       .complete(function(err) {
         done(err);
       });
@@ -20,6 +28,26 @@ describe('Question', function() {
           if (err) {
             throw err;
           };
+
+          done();
+        })
+    });
+
+    it('should create a question with the given title', function(done) {
+      Question
+        .create({ title: 'Example Question' })
+        .complete(function(err, question) {
+          if (err) {
+            throw err;
+          };
+
+          if (!question) {
+            throw new Exception('No question returned.');
+          }
+
+          if (!question.title) {
+            throw new Exception('Title missing from question.');
+          }
 
           done();
         })
