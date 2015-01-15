b0e865bae84d2d1d9f72554878a58977f0be245e
James Holder <github.com@thirdtruck.org>
2015-01-14 19:38:18 -0500

    Draft preliminary Backbone models and views.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,76 @@
-$.get('/questions/1', function(data) {
-  var question = data.question;
-  console.log(question);
-  $('.question').text(question.title);
-  $('.loading').hide();
-  $('.survey').show();
+(function() {
+'use strict';
+
+var Question = Backbone.Model.extend({
+
+  defaults: {
+    title: 'Title Missing',
+    answers: []
+  },
+
+  urlRoot: '/questions'
+
 });
+
+var Questions = Backbone.Collection.extend({
+
+  model: Question,
+
+  url: '/questions'
+
+});
+
+var QuestionView = Backbone.View.extend({
+
+  initialize: function() {
+    var view = this;
+    view.listenTo(view.model, "change", view.render);
+  },
+
+  render: function() {
+    var view = this;
+    view.$el.text(this.model.get('title'));
+  }
+
+});
+
+var LoadingView = Backbone.View.extend({
+
+  initialize: function() {
+    var view = this;
+
+    view.$loading = view.$el.find('.loading');
+    view.$survey = view.$el.find('.survey');
+
+    view.listenTo(view.model, "request", function() {
+      view.$loading.show();
+      view.$survey.hide();
+    });
+
+    view.listenTo(view.model, "sync", function() {
+      view.$loading.hide();
+      view.$survey.show();
+    });
+  }
+
+});
+
+$(document).ready(function() {
+
+  var question = new Question({ id: 1 });
+
+  var questionView = new QuestionView({
+    model: question,
+    el: $('.question')
+  });
+
+  var loadingView = new LoadingView({
+    model: question,
+    el: document
+  });
+
+  question.fetch();
+
+});
+
+})();
