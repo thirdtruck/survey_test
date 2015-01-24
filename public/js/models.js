4b737ee83db2adf22abcf7fe71eef90c554cb87a
James Holder <github.com@thirdtruck.org>
2015-01-24 17:12:07 -0500

    Move front-end model and view definitions into their own files.

---
+++ public/js/models.js
@@ -0,0 +1,87 @@
+var models;
+
+(function() {
+
+var User = Backbone.Model.extend({
+  
+  defaults: {
+    id: null,
+    uuid: null,
+    anonymous: undefined
+  }
+
+});
+
+var Question = Backbone.Model.extend({
+
+  defaults: {
+    title: 'Title Missing',
+    answers: []
+  },
+
+  urlRoot: '/questions',
+
+  parse: function(data, options) {
+    var question = this;
+
+    question.answers = new Answers(data.Answers || [], {
+      question: question
+    });
+    
+    return Backbone.Model.prototype.parse.apply(this, arguments);
+  }
+
+});
+
+var Questions = Backbone.Collection.extend({
+
+  model: Question,
+
+  url: '/questions'
+
+});
+
+var Answer = Backbone.Model.extend({
+  
+  defaults: {
+    title: 'Title Missing'
+  }
+
+});
+
+var Answers = Backbone.Collection.extend({
+  
+  model: Answer,
+
+  initialize: function() {
+    var answers = this;
+
+    answers.selected = null;
+
+    answers.on('answer_selected', answers.answerSelected, answers);
+  },
+
+  answerSelected: function(answer) {
+    var answers = this;
+
+    answers.selected = answer;
+  }
+
+});
+
+var Response = Backbone.Model.extend({
+  
+  urlRoot: '/responses'
+  
+});
+
+models = {
+  User: User,
+  Question: Question,
+  Questions: Questions,
+  Answer: Answer,
+  Answers: Answers,
+  Response: Response,
+};
+
+})();
